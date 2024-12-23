957b20dbe226de3b9e0a433259c5b87df232bec8
Hanru <haru.lertrante@gmail.com>
2024-12-23 14:06:02 +0800

    test offcanvas

--- wsm-web/src/assets/Components/Header/Navigator/Navigator.jsx
+++ wsm-web/src/assets/Components/Header/Navigator/Navigator.jsx
@@ -3,6 +3,7 @@ import './Navigator.css'
 
 const Navigator = () => {
     const nameShort = 'WSM'
+    const brandName = 'WAVING STRINGS MUSIC'
   return (
     <div>
         <nav className='navbar navbar-expand-xxl fixed-top'>
@@ -14,9 +15,16 @@ const Navigator = () => {
                         <i className='bx bx-search'></i>
                     </li>
                     <li className='nav-item'>
-                        <i className='bx bx-menu-alt-right'></i>
+                        <i className='bx bx-menu-alt-right' data-bs-toggle='offcanvas' data-bs-target='#menuNavbar' aria-controls='offcanvasExample'></i>
                     </li>
                 </ul>
+
+                <div class='offcanvas offcanvas-end' tabindex={-1} id='menuNavbar' aria-labelledby='offcanvasExampleLabel'>
+                    <div className='offcanvas-header'>
+                        <h6 className='offcanvas-title' id='offcanvasExampleLabel'>{brandName}</h6>
+                        <i className='bx bx-chevron-right ms-auto fs-3' data-bs-dismiss='offcanvas' aria-label='Close'></i>
+                    </div>
+                </div>
             </div>
         </nav>
     </div>
