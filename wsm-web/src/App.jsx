dec014c5f9770e06c4303374ecdeddf1082e34a6
Hanru <haru.lertrante@gmail.com>
2024-12-23 17:30:34 +0800

    carousel

--- wsm-web/src/App.jsx
+++ wsm-web/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navigator from './assets/Components/Header/Navigator/Navigator'
 import Banner from './assets/Components/Main/Banner/Banner'
+import Carousel from './assets/Components/Main/Carousel/Carousel'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,11 +12,16 @@ function App() {
       <header>
         <Navigator />
       </header>
+
       <main>
-        <div className='container-fluid mt-5 pt-5'>
+        <div className='container-fluid mt-5 mb-3 pt-5'>
           <Banner />
         </div>
+        <div className='container-fluid mt-5 mb-3'>
+          <Carousel />
+        </div>
       </main>
+
       <footer></footer>
     </>
   )
---
+++ wsm-web/src/assets/Components/Main/Carousel/Carousel.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Carousel.css'
+
+const firstSlide = 'https://res.cloudinary.com/debouhckk/image/upload/v1733304927/Waving%20Strings/suhr/pndgzccx28wk3rtldrza.png'
+const secondSlide = 'https://res.cloudinary.com/debouhckk/image/upload/v1733304927/Waving%20Strings/suhr/prwykh6zoirezaecprlz.png'
+const thirdSlide = 'https://res.cloudinary.com/debouhckk/image/upload/v1733303773/Waving%20Strings/suhr/hmmwo9cmsy3bwb2rsiz2.jpg'
+
+const Carousel = () => {
+
+    fetch(firstSlide).then(response => response.json()).then(data => console.log(data))
+    fetch(secondSlide).then(response => response.json()).then(data => console.log(data))
+    fetch(thirdSlide).then(response => response.json()).then(data => console.log(data))
+
+  return (
+    <div className='carouselle'>
+
+        <div className='carousel slide rounded' id="carouselExampleSlideOnly" data-bs-ride='carousel'>
+            <div className='carousel-indicators'>
+                <button className='active' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='0' aria-current='true' aria-label='Slide 1'></button>
+                <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='1' aria-label='Slide 2'></button>
+                <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='2' aria-label='Slide 3'></button>
+            </div>
+
+            <div className='carousel-inner'>
+                <div className='carousel-item active' data-bs-interval='3000'>
+                    <img src={firstSlide} className='d-block w-100 rounded' alt='slide 1' />
+                </div>
+                <div className='carousel-item active' data-bs-interval='3000'>
+                    <img src={secondSlide} className='d-block w-100 rounded' alt='slide 1' />
+                </div>
+                <div className='carousel-item active' data-bs-interval='3000'>
+                    <img src={thirdSlide} className='d-block w-100 rounded' alt='slide 1' />
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Carousel
