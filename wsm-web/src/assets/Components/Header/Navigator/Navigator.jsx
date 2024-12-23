5c36a7ec674810193c425785ae8ff2d09f102c1b
Hanru <haru.lertrante@gmail.com>
2024-12-23 16:23:37 +0800

    links

--- wsm-web/src/assets/Components/Header/Navigator/Navigator.jsx
+++ wsm-web/src/assets/Components/Header/Navigator/Navigator.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navigator.css'
 
 const Navigator = () => {
+
     const nameShort = 'WSM'
     const brandName = 'WAVING STRINGS MUSIC'
+
+    const[navFirstLink] = useState('HOME')
+    const[navSecondLink] = useState('PRODUCTS')
+    const[navThirdLink] = useState('SERVICES')
+    const[navFourthLink] = useState('CONTACTS')
+    const[navFifthLink] = useState('ABOUT US')
+
   return (
     <div>
         <nav className='navbar navbar-expand-xxl fixed-top'>
@@ -21,8 +29,33 @@ const Navigator = () => {
 
                 <div class='offcanvas offcanvas-end' tabindex={-1} id='menuNavbar' aria-labelledby='offcanvasExampleLabel'>
                     <div className='offcanvas-header'>
-                        <h6 className='offcanvas-title' id='offcanvasExampleLabel'>{brandName}</h6>
-                        <i className='bx bx-chevron-right ms-auto fs-3' data-bs-dismiss='offcanvas' aria-label='Close'></i>
+                        <h6 className='offcanvas-title rounded p-2' id='offcanvasExampleLabel'>{brandName}</h6>
+                        <i className='bx bx-chevron-right ms-auto fs-3 rounded' data-bs-dismiss='offcanvas' aria-label='Close'></i>
+                    </div>
+                    <div className='offcanvas-body'>
+                        <div className='row gx-1 text-center'>
+
+                            <div className='col-sm-12 mt-5 p-2'>
+                                <a className='nav-link' href="#">{navFirstLink}</a>
+                            </div>
+
+                            <div className='col-sm-12 mt-4 p-2'>
+                                <a className='nav-link' href="#">{navSecondLink}</a>
+                            </div>
+
+                            <div className='col-sm-12 mt-4 p-2'>
+                                <a className='nav-link' href="#">{navThirdLink}</a>
+                            </div>
+
+                            <div className='col-sm-12 mt-4 p-2'>
+                                <a className='nav-link' href="#">{navFourthLink}</a>
+                            </div>
+
+                            <div className='col-sm-12 mt-4 p-2'>
+                                <a className='nav-link' href="#">{navFifthLink}</a>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
